17ff44f1c38268f3d6108ede4482db7277347685
santhoshmkr <kalidosssanthosh09@gmail.com>
2024-06-10 22:51:25 +0530

    removed whatsapp

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,14 +25,7 @@ const Contact = () => {
           <div className=" text-2xl text-blue-500"><IoCall className=" m-0 "/></div>
           <span className=""><a href="tel:{CONTACT.phoneNo}" className="">{CONTACT.phoneNo}</a></span>
         </motion.div>
-        <motion.div 
-        whileInView={{ opacity: 1, x: 0 }}
-        initial={{ opacity: 0, x: 100 }}
-        transition={{ duration: 1.5 }}
-        className="flex gap-1 items-center my-4 justify-center">
-          <div className=" text-2xl text-green-500"><FaWhatsapp className=" m-0 "/></div>
-          <span className=""><a href="{whatsappLink}" className="">{CONTACT.whatsapp}</a></span>
-        </motion.div>
+        
         
         <a href="mailto:{CONTACT.email}" className="my-4 hover:border-b">{CONTACT.email}</a>
       </div>
