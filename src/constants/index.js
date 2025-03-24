5e92ca797bc1b3cca6584f73c563aa926a02aa16
Santhosh Mkr <kalidossSanthosh09@gmail.com>
2025-03-24 12:45:56 +0530

    updated all projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,12 @@
-import project1 from "../assets/projects/calender.png";
-import project2 from "../assets/projects/music-app.png";
-import project3 from "../assets/projects/portfolio.png";
+import dona from "../assets/projects/dona.png";
+import project2 from "../assets/projects/fascinato.png";
+// import project3 from "../assets/projects/portfolio.png";
 import on_off from "../assets/projects/on-off.png";
 import goat from "../assets/projects/goat.png";
+import jayadeep from "../assets/projects/jaydeep.png";
 import neelcord from "../assets/projects/neelcord.png";
+import Rathi from "../assets/projects/rathisilks.png";
+import mileShift from "../assets/projects/7th mile.png";
 
 export const HERO_CONTENT = `Dynamic and detail-oriented aspiring Frontend Developer with a strong foundation in designing, developing, and maintaining user-friendly web applications using React. Proficient in modern web technologies and passionate about crafting efficient and scalable web solutions. Demonstrates the ability to learn quickly and collaborate effectively with cross-functional teams. Eager to apply skills and passion for technology in a challenging and growth-oriented environment.`;
 
@@ -21,20 +24,20 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "EasyPlan:Your All-in-One Organizer",
-    image: project1,
-    live: "https://66608bd7cab85641da5f54bf--statuesque-stardust-b4017c.netlify.app/",
+    title: "Dona -organ donation website",
+    image: dona,
+    live: "https://donation-eight-olive.vercel.app/",
     description:
-      "EasyPlan merges calendar and tasks for streamlined organization and productivity. Stay focused effortlessly.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Dona is a comprehensive platform that simplifies the organ donation process by securely storing donor information, generating customizable donation forms for online completion or printing, and providing administrators with a dashboard to manage data, track registrations, and monitor activities.",
+    technologies: ["React", "tailwind", "Express", "MongoDB"],
   },
   {
-    title: "Music landing App",
+    title: "Fascinato",
     image: project2,
-    live:"#",
+    live:"https://fascinito.com/",
     description:
-      "An application for view the album and play the music.",
-    technologies: ["HTML", "CSS", "Javascript"],
+      "Fascinato is a specialized online platform offering a curated collection of baby clothing designed for newborns, infants, and toddlers. The website provides a convenient shopping experience for parents seeking quality garments for their little ones, featuring a range of styles from everyday essentials to special occasion wear. ",
+    technologies: ["Wordpress","elementor", "CSS", "PHP"],
   },
   {
     title: "on-off clothing website",
@@ -47,19 +50,43 @@ export const PROJECTS = [
   {
     title: "goat the legend clothing website",
     image: goat,
-    live:"https://goatthelegend.com/",
+    live:"#",
     description:
       "E-commerce website built on wordpress, featuring a user-friendly interface, secure checkout, and a wide range of customizable features.",
     technologies: ["wordpress","php","elementor", "CSS"],
   },
-  // {
-  //   title: "neelcord clothing website (b2b) under development",
-  //   image: neelcord,
-  //   live:"https://neelcord.com/",
-  //   description:
-  //     "B2B Catalog Website (WordPress) is a platform designed to showcase a company's product offerings to wholesale buyers, featuring a user-friendly interface, detailed product information, and easy-to-use navigation.",
-  //   technologies: ["wordpress","php","elementor", "CSS", "Javascript"],
-  // },
+  {
+    title: "neelcord clothing website (b2b) under development",
+    image: neelcord,
+    live:"#",
+    description:
+      "B2B Catalog Website (WordPress) is a platform designed to showcase a company's product offerings to wholesale buyers, featuring a user-friendly interface, detailed product information, and easy-to-use navigation.",
+    technologies: ["wordpress","php","elementor", "CSS", "Javascript"],
+  },
+  {
+    title: "jayadeep agro",
+    image: jayadeep,
+    live:"https://jaydeepagro.com/",
+    description:
+      "Jayadeep Agro is a prominent online platform specializing in high-quality nuts and dry fruits. Based in South India, particularly in Panruti, they are known for their premium cashew products and various other nuts",
+    technologies: ["wordpress","php","elementor", "CSS"],
+  },
+  {
+    title: "Rathi siks",
+    image: Rathi,
+    live:"https://jaydeepagro.com/",
+    description:
+      "Established in the year 1989, Rathi Silk & Sarees has been one of India’s leading manufacturers of wedding sarees and traditional women’s wear. Rathi Silks bring over 30 years of rich experience in crafting some of the most elegant, trendy and traditional designs in women’s wear. ",
+    technologies: ["wordpress","php","elementor", "CSS"],
+  },
+  {
+    title: "7th Mile Shift",
+    image: mileShift,
+    live:"#",
+    description:
+      "7th Mile Shift is a management consulting firm specializing in supply chain and logistics solutions. Based in Krishnagiri, Tamil Nadu, India, the company offers end-to-end supply chain consulting services, focusing on optimization, design, and efficiency improvements for businesses. They help organizations streamline their logistics operations, reduce costs, and enhance overall supply chain performance. ",
+    technologies: ["wordpress","php","elementor", "CSS"],
+  },
   
 ];
 
